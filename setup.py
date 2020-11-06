a429479da47a7a82e7c8109c108265adc2a7e990
Yixuan Xu <yixuan_xu@outlook.com>
2020-11-06 14:21:03 +0800

    [EHN] Add cleanup to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+from distutils.command.clean import clean as Clean
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
@@ -16,8 +19,40 @@ install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 
 cmdclass = {}
 
+
+# Custom clean command to remove build artifacts
+class CleanCommand(Clean):
+    description = "Remove build artifacts from the source tree"
+
+    def run(self):
+        Clean.run(self)
+        # Remove c files if we are not within a sdist package
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        remove_c_files = not os.path.exists(os.path.join(cwd, 'PKG-INFO'))
+        if remove_c_files:
+            print('Will remove generated .c files')
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+        for dirpath, dirnames, filenames in os.walk('sklearn'):
+            for filename in filenames:
+                if any(filename.endswith(suffix) for suffix in
+                       (".so", ".pyd", ".dll", ".pyc")):
+                    os.unlink(os.path.join(dirpath, filename))
+                    continue
+                extension = os.path.splitext(filename)[1]
+                if remove_c_files and extension in ['.c', '.cpp']:
+                    pyx_file = str.replace(filename, extension, '.pyx')
+                    if os.path.exists(os.path.join(dirpath, pyx_file)):
+                        os.unlink(os.path.join(dirpath, filename))
+            for dirname in dirnames:
+                if dirname == '__pycache__':
+                    shutil.rmtree(os.path.join(dirpath, dirname))
+
+cmdclass.update({'clean': CleanCommand})
+
+
 setup(
-    name='Ensemble-PyTorch',
+    name='torchensemble',
     version='0.0.1',
     author='AaronX121',
     
@@ -27,10 +62,17 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/AaronX121/Ensemble-Pytorch',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS'],
     keywords='Ensemble Learning',
     
     packages=find_packages(),
