3edb23fdb085e37e854fa056ff80e25252e23a5d
Yixuan Xu <yixuan_xu@outlook.com>
2020-11-06 09:42:09 +0800

    [ENH] Simplify examples

--- examples/classification_cifar10_cnn.py
+++ examples/classification_cifar10_cnn.py
@@ -1,14 +1,12 @@
 """ Example on classification using CIFAR-10. """
 
-import sys
-sys.path.append('../')
-
 import time
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from lenet5 import LeNet5
 from torchensemble.fusion import FusionClassifier
 from torchensemble.voting import VotingClassifier
 from torchensemble.bagging import BaggingClassifier
@@ -24,6 +22,42 @@ def display_records(records):
         print(msg.format(method, acc, training_time, evaluating_time))
 
 
+class LeNet5(nn.Module):
+    
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        
+        self.conv1 = nn.Conv2d(3, 6, 5, padding=2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(576, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, X):
+        
+        # CONV layers
+        output = F.max_pool2d(F.relu(self.conv1(X)), (2, 2))
+        output = F.max_pool2d(F.relu(self.conv2(output)), (2, 2))
+        output = output.view(-1, self.num_flat_features(output))
+        
+        # FC layers
+        output = F.relu(self.fc1(output))
+        output = F.dropout(output)
+        output = F.relu(self.fc2(output))
+        output = F.dropout(output)
+        output = self.fc3(output)
+        
+        return output
+   
+    def num_flat_features(self, X):
+        size = X.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        
+        return num_features
+
+
 if __name__ == '__main__':
     
     # Hyper-parameters
@@ -35,7 +69,7 @@ if __name__ == '__main__':
     
     # Utils
     batch_size = 128
-    data_dir = '../../Dataset/cifar'
+    data_dir = '../../Dataset/cifar'  # MODIFY THIS IF YOU WANT
     records = []
     torch.manual_seed(0)
     
