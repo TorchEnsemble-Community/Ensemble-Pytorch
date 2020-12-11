c993aed9a33bc438efe4ea5b1bf1aabf024e93d9
AaronX121 <xuyx@lamda.nju.edu.cn>
2020-12-11 15:32:26 +0800

    [ENH] Upload the documentation

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -1,2 +1,4 @@
 Change Log
 ==========
+
+TBA.
--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,9 @@ author = 'Yi-Xuan Xu'
 
 # -- General configuration ---------------------------------------------------
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
