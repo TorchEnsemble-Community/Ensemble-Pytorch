df2763c3f5f66841e520f4b9ade1a54a7512ba7a
xuyxu <xuyx@lamda.nju.edu.cn>
2021-01-01 17:31:58 +0800

    [DOC] Refactor docstrings

--- torchensemble/voting.py
+++ torchensemble/voting.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from joblib import Parallel, delayed
 
-from ._base import BaseModule, torchensemble_model_doc
+from ._base import BaseModule
 from . import utils
 
 
@@ -67,14 +67,60 @@ def _parallel_fit_per_epoch(train_loader,
     return estimator
 
 
-@torchensemble_model_doc("""Implementation on the VotingClassifier.""",
-                         "model")
 class VotingClassifier(BaseModule):
+    """
+    Implementation on the VotingClassifier.
 
-    @torchensemble_model_doc(
-        """Implementation on the data forwarding in VotingClassifier.""",
-        "classifier_forward")
+    Parameters
+    ----------
+    estimator : torch.nn.Module
+        The class of base estimator inherited from :mod:`torch.nn.Module`.
+    n_estimators : int
+        The number of base estimators in the ensemble.
+    estimator_args : dict, default=None
+        The dictionary of hyper-parameters used to instantiate base
+        estimators.
+    cuda : bool, default=True
+
+        - If ``True``, use GPU to train and evaluate the ensemble.
+        - If ``False``, use CPU to train and evaluate the ensemble.
+    n_jobs : int, default=None
+        The number of workers for training the ensemble. This
+        argument is used for parallel ensemble methods such as
+        :mod:`voting` and :mod:`bagging`. Setting it to an integer larger
+        than ``1`` enables a total number of ``n_jobs`` base estimators
+        to be trained simultaneously.
+    verbose : int, default=1
+        Control the level on printing logging information.
+
+        - If ``0``, trigger the silent mode
+        - If ``1``, basic logging information on the training and
+          evaluating status is printed.
+        - If ``> 1``, full logging information is printed.
+
+    Attributes
+    ----------
+    estimators_ : torch.nn.ModuleList
+        The internal container that stores all base estimators.
+
+    """
+
     def forward(self, X):
+        """
+        Implementation on the data forwarding in VotingClassifier.
+
+        Parameters
+        ----------
+        X : tensor
+            Input batch of data, which should be a valid input data batch for
+            base estimators.
+    
+        Returns
+        -------
+        proba : tensor of shape (batch_size, n_classes)
+            The predicted class distribution.
+
+        """
         batch_size = X.size()[0]
         proba = torch.zeros(batch_size, self.n_outputs).to(self.device)
 
@@ -84,9 +130,6 @@ class VotingClassifier(BaseModule):
 
         return proba
 
-    @torchensemble_model_doc(
-        """Implementation on the training stage of VotingClassifier.""",
-        "fit")
     def fit(self,
             train_loader,
             lr=1e-3,
@@ -97,6 +140,45 @@ class VotingClassifier(BaseModule):
             test_loader=None,
             save_model=True,
             save_dir=None):
+        """
+        Implementation on the training stage of VotingClassifier.
+
+        Parameters
+        ----------
+        train_loader : torch.utils.data.DataLoader
+            A :mod:`DataLoader` container that contains the training data.
+        lr : float, default=1e-3
+            The learning rate of the parameter optimizer.
+        weight_decay : float, default=5e-4
+            The weight decay of the parameter optimizer.
+        epochs : int, default=100
+            The number of training epochs.
+        optimizer : {"SGD", "Adam", "RMSprop"}, default="Adam"
+            The type of parameter optimizer.
+        log_interval : int, default=100
+            The number of batches to wait before printting the training status.
+        test_loader : torch.utils.data.DataLoader, default=None
+            A :mod:`DataLoader` container that contains the evaluating data.
+    
+            - If ``None``, no validation is conducted after each training
+              epoch.
+            - If not ``None``, the ensemble will be evaluated on this
+              dataloader after each training epoch.
+        save_model : bool, default=True
+            Whether to save the model.
+    
+            - If test_loader is ``None``, the ensemble trained over ``epochs``
+              will be saved.
+            - If test_loader is not ``None``, the ensemble with the best
+              validation performance will be saved.
+        save_dir : string, default=None
+            Specify where to save the model.
+    
+            - If ``None``, the model will be saved in the current directory.
+            - If not ``None``, the model will be saved in the specified
+              directory: ``save_dir``.
+
+        """
 
         # Instantiate base estimators and set attributes
         estimators = []
@@ -174,10 +256,20 @@ class VotingClassifier(BaseModule):
         if save_model and not test_loader:
             utils.save(self, save_dir, self.verbose)
 
-    @torchensemble_model_doc(
-        """Implementation on the evaluating stage of VotingClassifier.""",
-        "classifier_predict")
     def predict(self, test_loader):
+        """
+        Implementation on the evaluating stage of VotingClassifier.
+
+        Parameters
+        ----------
+        test_loader : torch.utils.data.DataLoader
+            A :mod:`DataLoader` container that contains the testing data.
+    
+        Returns
+        -------
+        accuracy : float
+            The testing accuracy of the fitted ensemble on the ``test_loader``.
+        """
         self.eval()
         correct = 0.
 
@@ -192,14 +284,60 @@ class VotingClassifier(BaseModule):
         return accuracy
 
 
-@torchensemble_model_doc("""Implementation on the VotingRegressor.""",
-                         "model")
 class VotingRegressor(BaseModule):
+    """
+    Implementation on the VotingRegressor.
 
-    @torchensemble_model_doc(
-        """Implementation on the data forwarding in VotingRegressor.""",
-        "regressor_forward")
+    Parameters
+    ----------
+    estimator : torch.nn.Module
+        The class of base estimator inherited from :mod:`torch.nn.Module`.
+    n_estimators : int
+        The number of base estimators in the ensemble.
+    estimator_args : dict, default=None
+        The dictionary of hyper-parameters used to instantiate base
+        estimators.
+    cuda : bool, default=True
+
+        - If ``True``, use GPU to train and evaluate the ensemble.
+        - If ``False``, use CPU to train and evaluate the ensemble.
+    n_jobs : int, default=None
+        The number of workers for training the ensemble. This
+        argument is used for parallel ensemble methods such as
+        :mod:`voting` and :mod:`bagging`. Setting it to an integer larger
+        than ``1`` enables a total number of ``n_jobs`` base estimators
+        to be trained simultaneously.
+    verbose : int, default=1
+        Control the level on printing logging information.
+
+        - If ``0``, trigger the silent mode
+        - If ``1``, basic logging information on the training and
+          evaluating status is printed.
+        - If ``> 1``, full logging information is printed.
+
+    Attributes
+    ----------
+    estimators_ : torch.nn.ModuleList
+        The internal container that stores all base estimators.
+
+    """
+
     def forward(self, X):
+        """
+        Implementation on the data forwarding in VotingRegressor.
+
+        Parameters
+        ----------
+        X : tensor
+            Input batch of data, which should be a valid input data batch for
+            base estimators.
+
+        Returns
+        -------
+        pred : tensor of shape (batch_size, n_outputs)
+            The predicted values.
+
+        """
         batch_size = X.size()[0]
         pred = torch.zeros(batch_size, self.n_outputs).to(self.device)
 
@@ -209,9 +347,6 @@ class VotingRegressor(BaseModule):
 
         return pred
 
-    @torchensemble_model_doc(
-        """Implementation on the training stage of VotingRegressor.""",
-        "fit")
     def fit(self,
             train_loader,
             lr=1e-3,
@@ -222,6 +357,45 @@ class VotingRegressor(BaseModule):
             test_loader=None,
             save_model=True,
             save_dir=None):
+        """
+        Implementation on the training stage of VotingRegressor.
+
+        Parameters
+        ----------
+        train_loader : torch.utils.data.DataLoader
+            A :mod:`DataLoader` container that contains the training data.
+        lr : float, default=1e-3
+            The learning rate of the parameter optimizer.
+        weight_decay : float, default=5e-4
+            The weight decay of the parameter optimizer.
+        epochs : int, default=100
+            The number of training epochs.
+        optimizer : {"SGD", "Adam", "RMSprop"}, default="Adam"
+            The type of parameter optimizer.
+        log_interval : int, default=100
+            The number of batches to wait before printting the training status.
+        test_loader : torch.utils.data.DataLoader, default=None
+            A :mod:`DataLoader` container that contains the evaluating data.
+    
+            - If ``None``, no validation is conducted after each training
+              epoch.
+            - If not ``None``, the ensemble will be evaluated on this
+              dataloader after each training epoch.
+        save_model : bool, default=True
+            Whether to save the model.
+    
+            - If test_loader is ``None``, the ensemble trained over ``epochs``
+              will be saved.
+            - If test_loader is not ``None``, the ensemble with the best
+              validation performance will be saved.
+        save_dir : string, default=None
+            Specify where to save the model.
+    
+            - If ``None``, the model will be saved in the current directory.
+            - If not ``None``, the model will be saved in the specified
+              directory: ``save_dir``.
+
+        """
 
         # Instantiate base estimators and set attributes
         estimators = []
@@ -298,10 +472,22 @@ class VotingRegressor(BaseModule):
         if save_model and not test_loader:
             utils.save(self, save_dir, self.verbose)
 
-    @torchensemble_model_doc(
-        """Implementation on the evaluating stage of VotingRegressor.""",
-        "regressor_predict")
     def predict(self, test_loader):
+        """
+        Implementation on the evaluating stage of VotingRegressor.
+
+        Parameters
+        ----------
+        test_loader : torch.utils.data.DataLoader
+            A :mod:`DataLoader` container that contains the testing data.
+    
+        Returns
+        -------
+        mse : float
+            The testing mean squared error of the fitted model on the
+            ``test_loader``.
+
+        """
         self.eval()
         mse = 0.
         criterion = nn.MSELoss()
