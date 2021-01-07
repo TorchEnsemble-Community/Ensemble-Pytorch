7ba2363ac359d20c177a12f6d9afdfe56d5ea892
xuyxu <xuyx@lamda.nju.edu.cn>
2021-01-07 16:56:13 +0800

    [ENH] Add adversarial training as an ensemble method

---
+++ torchensemble/adversarial_training.py
@@ -0,0 +1,10 @@
+"""
+  Adversarial training is able to improve the performance of the model
+  by treating the adversarial samples as the augmented data with the same
+  class as the original samples. Details are available in the Algorithm 1 of
+  the paper below.
+
+  Reference:
+      B. Lakshminarayanan, A. Pritzel, C. Blundell, Simple and Scalable
+      Predictive Uncertainty Estimation using Deep Ensembles, NIPS, 2017.
+"""
