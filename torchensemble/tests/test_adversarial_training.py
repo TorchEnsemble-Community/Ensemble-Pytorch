f5d41383c48b7b01900a0608c2b77de42877d68a
Yi-Xuan Xu <xuyx@lamda.nju.edu.cn>
2021-01-13 16:30:13 +0800

    Update unit tests

---
+++ torchensemble/tests/test_adversarial_training.py
@@ -0,0 +1,50 @@
+import torch
+import pytest
+import numpy as np
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+
+import torchensemble
+from torchensemble.utils import set_logger
+
+
+set_logger("pytest_adversarial_training")
+
+
+X_train = torch.Tensor(np.array(([1, 1],
+                                 [2, 2],
+                                 [3, 3],
+                                 [4, 4])))
+y_train_clf = torch.LongTensor(np.array(([0, 0, 1, 1])))
+
+
+# Base estimator
+class MLP_clf(nn.Module):
+    def __init__(self):
+        super(MLP_clf, self).__init__()
+        self.linear1 = nn.Linear(2, 2)
+        self.linear2 = nn.Linear(2, 2)
+
+    def forward(self, X):
+        X = X.view(X.size()[0], -1)
+        output = self.linear1(X)
+        output = self.linear2(output)
+        return output
+
+
+def test_adversarial_training_range():
+    """
+    This unit test checks the input range check in adversarial_training.py.
+    """
+    model = torchensemble.AdversarialTrainingClassifier(estimator=MLP_clf,
+                                                        n_estimators=2,
+                                                        cuda=False)
+
+    # Prepare data
+    train = TensorDataset(X_train, y_train_clf)
+    train_loader = DataLoader(train, batch_size=2)
+
+    # Training
+    with pytest.raises(ValueError) as excinfo:
+        model.fit(train_loader)
+    assert "input range of samples passed to adversarial" in str(excinfo.value)
--- torchensemble/tests/test_all_models.py
+++ torchensemble/tests/test_all_models.py
@@ -54,20 +54,20 @@ class MLP_reg(nn.Module):
 
 
 # Trainining data
-X_train = torch.Tensor(np.array(([1, 1],
-                                 [2, 2],
-                                 [3, 3],
-                                 [4, 4])))
+X_train = torch.Tensor(np.array(([0.1, 0.1],
+                                 [0.2, 0.2],
+                                 [0.3, 0.3],
+                                 [0.4, 0.4])))
 
 y_train_clf = torch.LongTensor(np.array(([0, 0, 1, 1])))
 y_train_reg = torch.FloatTensor(np.array(([0.1, 0.2, 0.3, 0.4])))
 y_train_reg = y_train_reg.view(-1, 1)
 
 # Testing data
-X_test = torch.Tensor(np.array(([5, 5],
-                                [6, 6],
-                                [7, 7],
-                                [8, 8])))
+X_test = torch.Tensor(np.array(([0.5, 0.5],
+                                [0.6, 0.6],
+                                [0.7, 0.7],
+                                [0.8, 0.8])))
 
 y_test_clf = torch.LongTensor(np.array(([1, 1, 0, 0])))
 y_test_reg = torch.FloatTensor(np.array(([0.5, 0.6, 0.7, 0.8])))
