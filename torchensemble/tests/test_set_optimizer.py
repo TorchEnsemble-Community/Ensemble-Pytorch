7f9b8003eafa1d8125e5880544c35baeee63efeb
Yi-Xuan Xu <xuyx@lamda.nju.edu.cn>
2021-01-20 20:05:06 +0800

    Add unit tests on `set_optimizer`

--- torchensemble/tests/test_set_optimizer.py
+++ torchensemble/tests/test_set_optimizer.py
@@ -1,7 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 20 19:15:29 2021
+import pytest
+import torchensemble
+import torch.nn as nn
 
-@author: Yi-Xuan Xu
-"""
 
+optimizer_list = ["Adadelta",
+                  "Adagrad",
+                  "Adam",
+                  "AdamW",
+                  "Adamax",
+                  "ASGD",
+                  "RMSprop",
+                  "Rprop",
+                  "SGD"]
+
+# Base estimator
+class MLP(nn.Module):
+
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.linear1 = nn.Linear(2, 2)
+        self.linear2 = nn.Linear(2, 2)
+
+    def forward(self, X):
+        X = X.view(X.size()[0], -1)
+        output = self.linear1(X)
+        output = self.linear2(output)
+        return output
+
+
+@pytest.mark.parametrize("optimizer_name", optimizer_list)
+def test_set_optimizer_normal(optimizer_name):
+    model = MLP()
+    torchensemble.utils.set_module.set_optimizer(model,
+                                                 optimizer_name,
+                                                 lr=1e-3)
+
+
+def test_set_optimizer_abnormal():
+    model = MLP()
+    with pytest.raises(NotImplementedError) as excinfo:
+        torchensemble.utils.set_module.set_optimizer(model, "Unknwon")
+    assert "Unknown name of the optimizer" in str(excinfo.value)
