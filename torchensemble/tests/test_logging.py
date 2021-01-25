64bb4b1b57d9bcf827d7ab08fb0cc35b531e6a69
Wenjie Zhang <mf1833099@smail.nju.edu.cn>
2021-01-25 15:30:35 +0800

    [MNT] Add unit tests on logging module (#32)
    * Create the pytest script for logging module
    
    * Fix warnings raised by flake8
    
    * formalize the name of my function
    
    * a little formatting
    
    Co-authored-by: zzzzwj <zwj@nju.edu.cn>
    Co-authored-by: xuyxu <xuyx@lamda.nju.edu.cn>

---
+++ torchensemble/tests/test_logging.py
@@ -0,0 +1,28 @@
+import pytest
+
+from torchensemble.utils.logging import set_logger
+
+
+def _record(logger):
+    logger.debug("Debug!")
+    logger.info("Info!")
+    logger.warning("Warning!")
+    logger.error("Error!")
+    logger.critical("Critical!")
+
+
+def test_logger():
+    logger = set_logger("Loglevel_DEBUG", "DEBUG")
+    _record(logger)
+    logger = set_logger("Loglevel_INFO", "INFO")
+    _record(logger)
+    logger = set_logger("Loglevel_WARNING", "WARNING")
+    _record(logger)
+    logger = set_logger("Loglevel_ERROR", "ERROR")
+    _record(logger)
+    logger = set_logger("Loglevel_CRITICAL", "CRITICAL")
+    _record(logger)
+
+    with pytest.raises(ValueError) as excinfo:
+        set_logger("Loglevel_INVALID", "INVALID")
+    assert("INVALID" in str(excinfo.value))
