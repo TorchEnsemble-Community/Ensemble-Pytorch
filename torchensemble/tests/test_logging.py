d20eaf3da67a7ab07ba123f42111a6ae94630f67
zzzzwj <zwj@nju.edu.cn>
2021-01-27 22:49:42 +0800

    Eliminate warnings produced by flake8

--- torchensemble/tests/test_logging.py
+++ torchensemble/tests/test_logging.py
@@ -1,23 +1,22 @@
 import time
-import pytest
 from multiprocessing import Process
 from torchensemble.utils.logging import MPLoggingServer, MPLoggingClient
 
 
 def _record(level):
     level = level.upper()
-    mp_server = MPLoggingServer("Loglevel_{}".format(level), 
+    mp_server = MPLoggingServer("Loglevel_{}".format(level),
                                 level)
     log_server = Process(target=mp_server.log)
     log_server.start()
-    
+
     log_client = MPLoggingClient()
     log_client.debug("Debug!")
     log_client.info("Info!")
     log_client.warn("Warn!")
     log_client.error("Error!")
     log_client.critical("Critical!")
-    
+
     time.sleep(1)
     log_server.terminate()
 
