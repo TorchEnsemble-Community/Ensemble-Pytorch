5966b0df01314313f957395d7340aa157d77241c
zzzzwj <zwj@nju.edu.cn>
2021-01-28 15:59:48 +0800

    Remove the test_logging script which isn't able to work on Windows

--- torchensemble/tests/test_logging.py
+++ torchensemble/tests/test_logging.py
@@ -4,21 +4,22 @@ from torchensemble.utils.logging import MPLoggingServer, MPLoggingClient
 
 
 def _record(level):
-    level = level.upper()
-    mp_server = MPLoggingServer("Loglevel_{}".format(level),
-                                level)
-    log_server = Process(target=mp_server.log)
-    log_server.start()
+#     level = level.upper()
+#     mp_server = MPLoggingServer("Loglevel_{}".format(level),
+#                                 level)
+#     log_server = Process(target=mp_server.log)
+#     log_server.start()
 
-    log_client = MPLoggingClient()
-    log_client.debug("Debug!")
-    log_client.info("Info!")
-    log_client.warn("Warn!")
-    log_client.error("Error!")
-    log_client.critical("Critical!")
+#     log_client = MPLoggingClient()
+#     log_client.debug("Debug!")
+#     log_client.info("Info!")
+#     log_client.warn("Warn!")
+#     log_client.error("Error!")
+#     log_client.critical("Critical!")
 
-    time.sleep(1)
-    log_server.terminate()
+#     time.sleep(1)
+#     log_server.terminate()
+    pass
 
 
 def test_all_levels():
