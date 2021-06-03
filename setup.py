4341216e8b1e7bf2f7b6395782e49a207dc2766a
xuyxu <xuyx@lamda.nju.edu.cn>
2021-06-03 12:58:55 +0800

    mnt: update package info

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords=["Deep Learning", "PyTorch", "Ensemble Learning"],
     packages=find_packages(),
