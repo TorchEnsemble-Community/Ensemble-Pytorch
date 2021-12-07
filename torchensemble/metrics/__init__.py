6bb7711c8c31647b2319795643d8ecb6428063f9
Yi-Xuan Xu <xuyx@lamda.nju.edu.cn>
2021-12-07 22:57:22 +0800

    update code

---
+++ torchensemble/metrics/__init__.py
@@ -0,0 +1,8 @@
+from ._metrics import accuracy_score
+from ._metrics import mean_squared_error
+
+
+__all__ = [
+    "accuracy_score",
+    "mean_squared_error",
+]
---
+++ torchensemble/metrics/_metrics.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+
+__all__ = []
+
+
+def accuracy_score():
+    pass
+
+def mean_squared_error():
+    pass
