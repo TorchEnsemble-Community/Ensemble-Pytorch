fix gradient boosting classifier and regressor for ReduceLROnPlateau

torchensemble/tests/test_set_scheduler.py
@@ -1,6 +1,7 @@
 import torch
 import pytest
 import torchensemble
+import numpy as np
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -10,7 +11,7 @@ all_reg = [
     torchensemble.FusionRegressor,
     torchensemble.VotingRegressor,
     torchensemble.BaggingRegressor,
-    # torchensemble.GradientBoostingRegressor,
+    torchensemble.GradientBoostingRegressor,
     # torchensemble.SnapshotEnsembleRegressor,
     torchensemble.AdversarialTrainingRegressor,
     # torchensemble.FastGeometricRegressor,
@@ -22,7 +23,7 @@ all_clf = [
     torchensemble.FusionClassifier,
     torchensemble.VotingClassifier,
     torchensemble.BaggingClassifier,
-    # torchensemble.GradientBoostingClassifier,
+    torchensemble.GradientBoostingClassifier,
     # torchensemble.SnapshotEnsembleClassifier,
     torchensemble.AdversarialTrainingClassifier,
     # torchensemble.FastGeometricClassifier,
@@ -57,16 +58,30 @@ class MLP_clf(nn.Module):
         return output
 
 
-X = torch.rand(2, 2)
-y = torch.rand(2, 2)
+# Training data
+X_train = torch.rand(2, 2)
+y_train_reg = torch.rand(2, 2)
+X_train_clf = torch.Tensor(
+    np.array(([0.1, 0.1], [0.2, 0.2], [0.3, 0.3], [0.4, 0.4]))
+)
+y_train_clf = torch.LongTensor(np.array(([0, 0, 1, 1])))
 
+# Testing data
 X_test = torch.rand(2, 2)
-y_test = torch.rand(2, 2)
+y_test_reg = torch.rand(2, 2)
+numpy_X_test = np.array(([0.5, 0.5], [0.6, 0.6]))
+X_test_clf = torch.Tensor(numpy_X_test)
+y_test_clf = torch.LongTensor(np.array(([1, 0])))
 
-train_data = TensorDataset(X, y)
-train_loader = DataLoader(train_data, batch_size=5, shuffle=True)
-test_data = TensorDataset(X_test, y_test)
-test_loader = DataLoader(test_data, batch_size=5, shuffle=True)
+train_data_reg = TensorDataset(X_train, y_train_reg)
+train_loader_reg = DataLoader(train_data_reg, batch_size=5, shuffle=True)
+test_data_reg = TensorDataset(X_test, y_test_reg)
+test_loader_reg = DataLoader(test_data_reg, batch_size=5, shuffle=True)
+
+train_data_clf = TensorDataset(X_train_clf, y_train_clf)
+train_loader_clf = DataLoader(train_data_clf, batch_size=5, shuffle=True)
+test_data_clf = TensorDataset(X_test_clf, y_test_clf)
+test_loader_clf = DataLoader(test_data_clf, batch_size=5, shuffle=True)
 
 
 def test_set_scheduler_LambdaLR():
@@ -139,10 +154,10 @@ def test_set_scheduler_ReduceLROnPlateau():
     ],
 )
 @pytest.mark.parametrize(
-    "test_dataloader", [test_loader, None]
+    "test_dataloader", [test_loader_reg, None]
 )  # ReduceLROnPlateau scheduling should work when there is no test data
 @pytest.mark.parametrize(
-    "n_estimators", [1, 10]
+    "n_estimators", [1, 5]
 )  # LR scheduling works for 1 as well as many estimators
 @pytest.mark.parametrize("ensemble_model", all_reg)
 def test_fit_w_all_schedulers(
@@ -155,7 +170,10 @@ def test_fit_w_all_schedulers(
     model.set_optimizer("Adam", lr=1e-1)
     model.set_scheduler(**scheduler_dict)
     model.fit(
-        train_loader, epochs=50, test_loader=test_dataloader, save_model=False
+        train_loader_reg,
+        epochs=50,
+        test_loader=test_dataloader,
+        save_model=False,
     )
 
 
@@ -171,10 +189,10 @@ def test_fit_w_all_schedulers(
     ],
 )
 @pytest.mark.parametrize(
-    "test_dataloader", [test_loader, None]
+    "test_dataloader", [test_loader_clf, None]
 )  # ReduceLROnPlateau scheduling should work when there is no test data
 @pytest.mark.parametrize(
-    "n_estimators", [1, 10]
+    "n_estimators", [1, 5]
 )  # LR scheduling works for 1 as well as many estimators
 @pytest.mark.parametrize("ensemble_model", all_clf)
 def test_fit_w_all_schedulers_clf(
@@ -187,7 +205,10 @@ def test_fit_w_all_schedulers_clf(
     model.set_optimizer("Adam", lr=1e-1)
     model.set_scheduler(**scheduler_dict)
     model.fit(
-        train_loader, epochs=50, test_loader=test_dataloader, save_model=False
+        train_loader_clf,
+        epochs=50,
+        test_loader=test_dataloader,
+        save_model=False,
     )
 
 