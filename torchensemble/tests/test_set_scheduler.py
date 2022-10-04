0731e445cab34e1714bdb358229dbf7959ff3059
SarthakJariwala <35085572+SarthakJariwala@users.noreply.github.com>
2022-10-04 04:35:46 -0700

    fix: `ReduceLROnPlateau` scheduler for all regressors and classifiers (#128)
    * fix voting classifier and regressor to work with ReduceLROnPlateau scheduler
    
    * fix soft gradient boosting classifier and regressor to work with ReduceLROnPlateau scheduler
    
    * add tests for set_scheduler that test call the schedulers while fitting
    
    * fix adversarial classifier and regressor to work with ReduceLROnPlateau scheduler
    
    * set save_model to False for tests
    
    * fix bagging classifier and regressor to work with ReduceLROnPlateau scheduler
    
    * clear formatting errors
    
    * fix fusion classifier and regressor for ReduceLROnPlateau
    
    * fix code quality errors
    
    * fix gradient boosting classifier and regressor for ReduceLROnPlateau
    
    * enable SoftGradientBoostingClassifier test with scheduler
    
    * fix fast geometric classifier and regressor for ReduceLROnPlateau

--- torchensemble/tests/test_set_scheduler.py
+++ torchensemble/tests/test_set_scheduler.py
@@ -1,7 +1,32 @@
 import torch
 import pytest
 import torchensemble
+import numpy as np
 import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+
+
+# All regressors
+all_reg = [
+    torchensemble.FusionRegressor,
+    torchensemble.VotingRegressor,
+    torchensemble.BaggingRegressor,
+    torchensemble.GradientBoostingRegressor,
+    torchensemble.AdversarialTrainingRegressor,
+    torchensemble.FastGeometricRegressor,
+    torchensemble.SoftGradientBoostingRegressor,
+]
+
+# All classifiers
+all_clf = [
+    torchensemble.FusionClassifier,
+    torchensemble.VotingClassifier,
+    torchensemble.BaggingClassifier,
+    torchensemble.GradientBoostingClassifier,
+    torchensemble.AdversarialTrainingClassifier,
+    torchensemble.FastGeometricClassifier,
+    torchensemble.SoftGradientBoostingClassifier,
+]
 
 
 # Base estimator
@@ -16,6 +41,45 @@ class MLP(nn.Module):
         output = self.linear1(X)
         output = self.linear2(output)
         return output
+
+
+class MLP_clf(nn.Module):
+    def __init__(self):
+        super(MLP_clf, self).__init__()
+        self.linear1 = nn.Linear(2, 2)
+        self.linear2 = nn.Linear(2, 2)
+
+    def forward(self, X):
+        X = X.view(X.size()[0], -1)
+        output = self.linear1(X)
+        output = self.linear2(output)
+        return output
+
+
+# Training data
+X_train = torch.rand(2, 2)
+y_train_reg = torch.rand(2, 2)
+X_train_clf = torch.Tensor(
+    np.array(([0.1, 0.1], [0.2, 0.2], [0.3, 0.3], [0.4, 0.4]))
+)
+y_train_clf = torch.LongTensor(np.array(([0, 0, 1, 1])))
+
+# Testing data
+X_test = torch.rand(2, 2)
+y_test_reg = torch.rand(2, 2)
+numpy_X_test = np.array(([0.5, 0.5], [0.6, 0.6]))
+X_test_clf = torch.Tensor(numpy_X_test)
+y_test_clf = torch.LongTensor(np.array(([1, 0])))
+
+train_data_reg = TensorDataset(X_train, y_train_reg)
+train_loader_reg = DataLoader(train_data_reg, batch_size=5, shuffle=True)
+test_data_reg = TensorDataset(X_test, y_test_reg)
+test_loader_reg = DataLoader(test_data_reg, batch_size=5, shuffle=True)
+
+train_data_clf = TensorDataset(X_train_clf, y_train_clf)
+train_loader_clf = DataLoader(train_data_clf, batch_size=5, shuffle=True)
+test_data_clf = TensorDataset(X_test_clf, y_test_clf)
+test_loader_clf = DataLoader(test_data_clf, batch_size=5, shuffle=True)
 
 
 def test_set_scheduler_LambdaLR():
@@ -73,6 +137,76 @@ def test_set_scheduler_ReduceLROnPlateau():
     optimizer = torch.optim.Adam(model.parameters())
     torchensemble.utils.set_module.set_scheduler(
         optimizer, "ReduceLROnPlateau"
+    )
+
+
+@pytest.mark.parametrize(
+    "scheduler_dict",
+    [
+        {"scheduler_name": "ReduceLROnPlateau", "patience": 2, "min_lr": 1e-6},
+        {"scheduler_name": "LambdaLR", "lr_lambda": lambda x: x * 0.1},
+        {"scheduler_name": "StepLR", "step_size": 30},
+        {"scheduler_name": "MultiStepLR", "milestones": [20, 40]},
+        {"scheduler_name": "ExponentialLR", "gamma": 0.1},
+        {"scheduler_name": "CosineAnnealingLR", "T_max": 100},
+    ],
+)
+@pytest.mark.parametrize(
+    "test_dataloader", [test_loader_reg, None]
+)  # ReduceLROnPlateau scheduling should work when there is no test data
+@pytest.mark.parametrize(
+    "n_estimators", [1, 5]
+)  # LR scheduling works for 1 as well as many estimators
+@pytest.mark.parametrize("ensemble_model", all_reg)
+def test_fit_w_all_schedulers(
+    scheduler_dict, test_dataloader, n_estimators, ensemble_model
+):
+    """Test if LR schedulers work when `fit` is called."""
+    model = ensemble_model(
+        estimator=MLP, n_estimators=n_estimators, cuda=False
+    )
+    model.set_optimizer("Adam", lr=1e-1)
+    model.set_scheduler(**scheduler_dict)
+    model.fit(
+        train_loader_reg,
+        epochs=50,
+        test_loader=test_dataloader,
+        save_model=False,
+    )
+
+
+@pytest.mark.parametrize(
+    "scheduler_dict",
+    [
+        {"scheduler_name": "ReduceLROnPlateau", "patience": 2, "min_lr": 1e-6},
+        {"scheduler_name": "LambdaLR", "lr_lambda": lambda x: x * 0.1},
+        {"scheduler_name": "StepLR", "step_size": 30},
+        {"scheduler_name": "MultiStepLR", "milestones": [20, 40]},
+        {"scheduler_name": "ExponentialLR", "gamma": 0.1},
+        {"scheduler_name": "CosineAnnealingLR", "T_max": 100},
+    ],
+)
+@pytest.mark.parametrize(
+    "test_dataloader", [test_loader_clf, None]
+)  # ReduceLROnPlateau scheduling should work when there is no test data
+@pytest.mark.parametrize(
+    "n_estimators", [1, 5]
+)  # LR scheduling works for 1 as well as many estimators
+@pytest.mark.parametrize("ensemble_model", all_clf)
+def test_fit_w_all_schedulers_clf(
+    scheduler_dict, test_dataloader, n_estimators, ensemble_model
+):
+    """Test if LR schedulers work when `fit` is called."""
+    model = ensemble_model(
+        estimator=MLP_clf, n_estimators=n_estimators, cuda=False
+    )
+    model.set_optimizer("Adam", lr=1e-1)
+    model.set_scheduler(**scheduler_dict)
+    model.fit(
+        train_loader_clf,
+        epochs=50,
+        test_loader=test_dataloader,
+        save_model=False,
     )
 
 
